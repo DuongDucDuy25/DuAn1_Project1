1c24bb959717d0afca005711c841150368b54587
Pro1121Nhom3 <duyddph44929@fpt.edu.vn>
2024-03-17 13:35:33 +0700

    manhhinhchinh

---
+++ app/src/main/java/fpoly/htdshoes_pro1121/fragment/frg_taikhoan.java
@@ -0,0 +1,29 @@
+package fpoly.htdshoes_pro1121.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import fpoly.htdshoes_pro1121.R;
+
+
+public class frg_taikhoan extends Fragment {
+
+
+
+    public frg_taikhoan() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frg_taikhoan, container, false);
+    }
+}
