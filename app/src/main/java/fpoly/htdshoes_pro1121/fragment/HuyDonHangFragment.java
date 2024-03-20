8587278350aec32231e0dc27e5e93275df3f6314
DuAn1_Project1 <duyddph44929@fpt.edu.vn>
2024-03-20 23:34:38 +0700

    config

---
+++ app/src/main/java/fpoly/htdshoes_pro1121/fragment/HuyDonHangFragment.java
@@ -0,0 +1,23 @@
+package fpoly.htdshoes_pro1121.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fpoly.htdshoes_pro1121.R;
+
+
+public class HuyDonHangFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_huy_don_hang, container, false);
+    }
+}
--- app/src/main/java/fpoly/htdshoes_pro1121/fragment/frg_donhang.java
+++ app/src/main/java/fpoly/htdshoes_pro1121/fragment/frg_donhang.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import fpoly.htdshoes_pro1121.R;
 
 
 public class frg_donhang extends Fragment {
+    private TableLayout mTabLayout;
+
+    private ViewPager viewPager;
 
 
     public frg_donhang() {
@@ -22,7 +27,9 @@ public class frg_donhang extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frg_donhang, container, false);
+        View view = inflater.inflate(R.layout.fragment_frg_donhang, container, false);
+        mTabLayout = view.findViewById(R.id.TabLayout);
+        viewPager = view.findViewById(R.id.viewpager);
+        return view;
     }
 }
