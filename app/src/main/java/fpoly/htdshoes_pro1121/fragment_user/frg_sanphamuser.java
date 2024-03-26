1061ced28f77d97667708a6684681b2271804f24
your name <abc@gmail.com>
2024-03-26 09:46:01 +0700

    SP_User

--- app/src/main/java/fpoly/htdshoes_pro1121/fragment_user/frg_sanphamuser.java
+++ app/src/main/java/fpoly/htdshoes_pro1121/fragment_user/frg_sanphamuser.java
@@ -1,66 +1,169 @@
 package fpoly.htdshoes_pro1121.fragment_user;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import fpoly.htdshoes_pro1121.AdapterAdmin.SanPhamAdminAdapter;
+import fpoly.htdshoes_pro1121.Dao.SanPhamDao;
+import fpoly.htdshoes_pro1121.Model.SanPham;
 import fpoly.htdshoes_pro1121.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link frg_sanphamuser#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class frg_sanphamuser extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView rcSanPham;
+    private SanPhamDao dao;
+    private SearchView searchView;
+    private LinearLayout btnThemSP, btnSuaSp;
+    private ArrayList<SanPham> list;
 
     public frg_sanphamuser() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment frg_sanphamuser.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static frg_sanphamuser newInstance(String param1, String param2) {
-        frg_sanphamuser fragment = new frg_sanphamuser();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frg_sanphamuser, container, false);
+        View view = inflater.inflate(R.layout.fragment_frg_sanpham, container, false);
+
+        rcSanPham = view.findViewById(R.id.rcSanPham);
+        btnThemSP = view.findViewById(R.id.btnThemSanPham);
+
+        dao = new SanPhamDao(getContext());
+        loadData();
+        btnThemSP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDiaLogAdd();
+            }
+        });
+
+        return view;
+    }
+
+    private void loadData() {
+        list = dao.getlistdata();
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        rcSanPham.setLayoutManager(manager);
+        SanPhamAdminAdapter adapter = new SanPhamAdminAdapter(getContext(), list, dao);
+        rcSanPham.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                ArrayList<SanPham> filteredList = new ArrayList<>();
+
+                for (SanPham sanPham : list) {
+                    if (sanPham.getTenSanPham().toLowerCase().contains(newText.toLowerCase())) {
+                        filteredList.add(sanPham);
+                    }
+                }
+
+                SanPhamAdminAdapter adapter = new SanPhamAdminAdapter(getContext(), filteredList, dao) {
+                    @Override
+                    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+                        super.onBindViewHolder(holder, position);
+                        holder.itemView.setBackgroundColor(Color.TRANSPARENT);
+                    }
+                };
+
+                rcSanPham.setAdapter(adapter);
+
+                return true;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void showDiaLogAdd() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_addsanpham, null);
+        builder.setView(view);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        EditText edTenSanPham = view.findViewById(R.id.edNameSanPham);
+        EditText edMaCTSP = view.findViewById(R.id.edMaCTSP);
+        EditText edMaTL = view.findViewById(R.id.edMaTL);
+        EditText edSoLuong = view.findViewById(R.id.edSoLuong);
+        EditText edGiaTien = view.findViewById(R.id.edGiaTien);
+        Button btnAdd = view.findViewById(R.id.btnAddSanPham);
+        Button btnCancel = view.findViewById(R.id.btnCancelSanPham);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ten = edTenSanPham.getText().toString();
+                String mactsp = edMaCTSP.getText().toString();
+                String matl = edMaTL.getText().toString();
+                String soluong = edSoLuong.getText().toString();
+                String giatien = edGiaTien.getText().toString();
+
+                if (ten.isEmpty() || mactsp.isEmpty() || matl.isEmpty() || soluong.isEmpty()) {
+                    Toast.makeText(getContext(), "Nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                } else {
+                    SanPham sanpham = new SanPham(ten, Integer.parseInt(mactsp), Integer.parseInt(matl), Integer.parseInt(soluong), Integer.parseInt(giatien));
+                    boolean check = dao.addSanPham(sanpham);
+                    if (check) {
+                        Toast.makeText(getContext(), "Thêm thành công!", Toast.LENGTH_SHORT).show();
+                        loadData();
+                        alertDialog.dismiss();
+                    } else {
+                        Toast.makeText(getContext(), "Thêm thất bại", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
     }
 }
